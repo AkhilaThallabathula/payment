f3614a4e3372be4ab16503f8d54800cacb221030
AkhilaThallabathula <146066134+AkhilaThallabathula@users.noreply.github.com>
2023-11-21 13:49:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function stringNumber(){
+    
+
+let name="Akhila";
+let number=7989490939;
+   a=(name+" : "+number);
+    document.getElementById("akki1").textContent=a;
+    
+
+    
+    }
+
+function BooleanTrueFalse(){
+    const isTodayThursday=true;
+    b=isTodayThursday;
+    document.getElementById("akki2").textContent=b;
+}
+ 
+function change(){
+
+let myArray=["yesterday"];
+    c=myArray;
+
+document.getElementById("akki3").textContent=c;
+
+}
